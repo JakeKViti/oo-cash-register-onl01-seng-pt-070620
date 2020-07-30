5f220adb0267d7f55b1c37824b3aa532dab748e1
Jacob Viti <thevitij64@gmail.com>
2020-07-30 01:43:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,7 @@ class CashRegister
    if !@discount
      return "There is no discount to apply."
    end
-    return "After the discount, the total comes to $#{total}"
+    return "After the discount, the total comes to $#{@discount}"
 
   end
 
